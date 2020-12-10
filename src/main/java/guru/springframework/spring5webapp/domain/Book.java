4da8dd8521a1c4392cd8fffafbca1d312e81e16b
我爱吃辣条 <>
2020-12-10 06:01:54 -0600

    demo1

---
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -0,0 +1,12 @@
+package guru.springframework.spring5webapp.domain;
+
+/**
+ * @author: Sharon
+ * @create: 2020-12-10
+ **/
+
+public class Book {
+
+    private String title;
+    private String isbn;
+}
